fb7eb72742abe6ee767bd37fd868de9f56aa0aeb
Steve Kingston <steve.kingston@os.uk>
2024-09-25 15:35:34 +0100

    feat: add bng resolution mappings
    Maps BNG resolutions from metres to string representations.

--- bng_reference.py
+++ bng_reference.py
@@ -1,7 +1,6 @@
 """
 """
 
-
 import re
 
 # Compile regular expression pattern for BNG reference
@@ -9,3 +8,17 @@ _pattern = re.compile(
     r"^[HJNOST][A-HJ-Z](\d{2}|\d{4}|\d{6}|\d{8}|\d{10})?(NE|SE|SW|NW)?$"
 )
 
+# BNG resolution mappings from metres to string representations
+_resolution_to_string = {
+    100000: "100km",
+    50000: "50km",
+    10000: "10km",
+    5000: "5km",
+    1000: "1km",
+    500: "500m",
+    100: "100m",
+    50: "50m",
+    10: "10m",
+    5: "5m",
+    1: "1m",
+}
