2bfdc83e8d53b70d991c55ff92ea2b969aba5c23
Steve Kingston <steve.kingston@os.uk>
2024-09-26 08:20:57 +0100

    feat: add utils module providing project utility functions

---
+++ osbng/utils.py
@@ -0,0 +1,16 @@
+"""Project utility functions."""
+
+import json
+
+
+def load_test_cases(file_path: str) -> dict:
+    """Load test cases from a JSON file
+
+    Args:
+        file_path (str): The path to the JSON file containing the test cases.
+
+    Returns:
+        dict: The test cases as a dictionary.
+    """
+    with open(file_path) as f:
+        return json.load(f)
