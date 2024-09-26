ccae1f13c6fc32275cca610fd7a04abd3a1b71e6
Steve Kingston <steve.kingston@os.uk>
2024-09-26 08:31:50 +0100

    test: add test_bng_reference module providing testing for the bng_reference module

---
+++ osbng/tests/test_bng_reference.py
@@ -0,0 +1,25 @@
+"""Testing for the bng_reference module.
+
+Test cases are loaded from a JSON file using the load_test_cases function from the utils module.
+"""
+
+import pytest
+
+from osbng.bng_reference import is_valid_bng
+from osbng.utils import load_test_cases
+
+
+# Parameterised test for is_valid_bng function
+@pytest.mark.parametrize(
+    "test_case",
+    load_test_cases(file_path="./data/bng_reference_test_cases.json")["is_valid_bng"],
+)
+def test_is_valid_bng(test_case):
+    """Test is_valid_bng function with test cases from JSON file.
+
+    Args:
+        test_case (dict): Test case from JSON file.
+    """
+    bng_ref_string = test_case["bng_ref_string"]
+    expected = test_case["expected"]
+    assert is_valid_bng(bng_ref_string) == expected
