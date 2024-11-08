868eb24f2a5b255237ee0d5168a39b96a075e398
Steve Kingston <steve.kingston@os.uk>
2024-11-08 14:29:10 +0000

    docs: update test documentation to reflect _is_valid_bng function name change

--- tests/test_bng_reference.py
+++ tests/test_bng_reference.py
@@ -9,13 +9,13 @@ from osbng.bng_reference import _is_valid_bng, _get_bng_resolution
 from osbng.utils import load_test_cases
 
 
-# Parameterised test for is_valid_bng function
+# Parameterised test for _is_valid_bng function
 @pytest.mark.parametrize(
     "test_case",
     load_test_cases(file_path="./data/bng_reference_test_cases.json")["_is_valid_bng"],
 )
 def test__is_valid_bng(test_case):
-    """Test is_valid_bng function with test cases from JSON file.
+    """Test _is_valid_bng function with test cases from JSON file.
 
     Args:
         test_case (dict): Test case from JSON file.
