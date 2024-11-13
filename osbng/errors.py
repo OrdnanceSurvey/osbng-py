3165804b49ab3223241375b9a9ceb4efb0ee1e50
Steve Kingston <steve.kingston@os.uk>
2024-11-13 12:04:34 +0000

    feat: add errors module providing custom exceptions

---
+++ osbng/errors.py
@@ -0,0 +1,7 @@
+"""Custom exceptions supporting interactions with the British National Grid (BNG)."""
+
+
+class BNGReferenceError(Exception):
+    """Exception rasied for errors in BNG references."""
+
+    pass
