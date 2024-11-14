d7fbe3e69bc4899cbec0845d10823bbc22d9c805
kat-1218 <root@ND51989.ordsvy.gov.uk>
2024-11-14 15:20:35 +0000

    feat: add custom bngresolutionerror exception to support resolution input handling

--- osbng/errors.py
+++ osbng/errors.py
@@ -1,7 +1,24 @@
 """Custom exceptions supporting interactions with the British National Grid (BNG)."""
 
+from osbng.resolution import _resolution_to_string
+
 
 class BNGReferenceError(Exception):
     """Exception rasied for errors in BNG references."""
 
     pass
+
+
+class BNGResolutionError(Exception):
+    """Exception raised for unsupported BNG resolutions."""
+
+    def __init__(self):
+        # Extract the numeric and string resolutions from _resolution_to_string
+        # Create message listing supported resolutions
+        message = (
+            "Invalid BNG resolution provided. Supported resolutions are: \n"
+            f"Metres: {", ".join(map(str, _resolution_to_string.keys()))}\n"
+            f"Labels: {", ".join(_resolution_to_string.values())}"
+        )
+        # Pass message to base class
+        super().__init__(message)
