d13fd307276251aef37b6706c6ca29afabc91fe9
kat-1218 <kate.new@os.uk>
2024-11-14 16:25:12 +0000

    test: add testing for _validate_and_normalise_bng_resolution function

---
+++ tests/test_indexing.py
@@ -0,0 +1,24 @@
+"""Testing for the indexing module.
+
+Test cases are loaded from a JSON file using the load_test_cases function from the utils module.
+"""
+
+import pytest
+
+from osbng.indexing import _validate_and_normalise_bng_resolution
+from osbng.errors import BNGResolutionError
+from osbng.utils import load_test_cases
+
+
+# Parameterised test for _validate_and_normalise_bng_resolution function
+@pytest.mark.parametrize(
+    "test_case",
+    load_test_cases(file_path="./data/indexing_test_cases.json")[
+        "_validate_and_normalise_bng_resolution"
+    ],
+)
+def test_bngreference(test_case):
+    """Test _validate_and_normalise_bng_resolution with test cases from JSON file."""
+    resolution = test_case["resolution"]
+    expected = test_case["expected"]
+    assert _validate_and_normalise_bng_resolution(resolution) == expected
