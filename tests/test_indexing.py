15a8afbbb0d79ae93cae84c3fbab34ce92837035
Steve Kingston <steve.kingston@os.uk>
2024-11-27 16:08:47 +0000

    test: update testing for _validate_and_normalise_bng_resolution function to cover custom exception error handling

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -6,7 +6,7 @@ Test cases are loaded from a JSON file using the load_test_cases function from t
 import pytest
 
 from osbng.indexing import _validate_and_normalise_bng_resolution
-from osbng.errors import BNGResolutionError
+from osbng.errors import BNGResolutionError, EXCEPTION_MAP
 from osbng.utils import load_test_cases
 
 
@@ -20,5 +20,13 @@ from osbng.utils import load_test_cases
 def test__validate_and_normalise_bng_resolution(test_case):
     """Test _validate_and_normalise_bng_resolution with test cases from JSON file."""
     resolution = test_case["resolution"]
-    expected = test_case["expected"]
-    assert _validate_and_normalise_bng_resolution(resolution) == expected
+    if "expected_exception" in test_case:
+        exception_name = test_case["expected_exception"]["name"]
+        # Get exception class from name
+        exception_class = EXCEPTION_MAP[exception_name]
+        with pytest.raises(exception_class):
+            _validate_and_normalise_bng_resolution(resolution)
+    else:
+        assert (
+            _validate_and_normalise_bng_resolution(resolution) == test_case["expected"]
+        )
