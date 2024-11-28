45148b9e5655af5d37b8bb06e8463328fc2ce9f4
Steve Kingston <steve.kingston@os.uk>
2024-11-28 09:53:22 +0000

    test: add testing for _validate_easting_northing to test_indexing

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -5,7 +5,10 @@ Test cases are loaded from a JSON file using the load_test_cases function from t
 
 import pytest
 
-from osbng.indexing import _validate_and_normalise_bng_resolution
+from osbng.indexing import (
+    _validate_and_normalise_bng_resolution,
+    _validate_easting_northing,
+)
 from osbng.errors import BNGResolutionError, EXCEPTION_MAP
 from osbng.utils import load_test_cases
 
@@ -30,3 +33,24 @@ def test__validate_and_normalise_bng_resolution(test_case):
         assert (
             _validate_and_normalise_bng_resolution(resolution) == test_case["expected"]
         )
+
+
+# Parameterised test for _validate_easting_northing function
+@pytest.mark.parametrize(
+    "test_case",
+    load_test_cases(file_path="./data/indexing_test_cases.json")[
+        "_validate_easting_northing"
+    ],
+)
+def test__validate_easting_northing(test_case):
+    """Test _validate_and_normalise_bng_resolution with test cases from JSON file."""
+    easting = test_case["easting"]
+    northing = test_case["northing"]
+    if "expected_exception" in test_case:
+        exception_name = test_case["expected_exception"]["name"]
+        # Get exception class from name
+        exception_class = EXCEPTION_MAP[exception_name]
+        with pytest.raises(exception_class):
+            _validate_easting_northing(easting, northing)
+    else:
+        _validate_easting_northing(easting, northing)
