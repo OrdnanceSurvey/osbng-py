ba66ac4176eff7401d27b684c4398d76b240c17e
kat-1218 <kate.new@os.uk>
2025-01-29 09:22:04 +0000

    test: add testing for bng_to_parent to test_hierarchy

--- tests/test_hierarchy.py
+++ tests/test_hierarchy.py
@@ -42,3 +42,35 @@ def test__bng_to_children(test_case):
 
         for bng, expected in zip(bng_refs, test_case["expected"]):
             assert bng.bng_ref_formatted == expected["bng_ref_formatted"]
+
+
+@pytest.mark.parametrize(
+    "test_case",
+    load_test_cases(file_path="./data/hierarchy_test_cases.json")["bng_to_parent"],
+)
+def test__bng_to_parent(test_case):
+    """Test bng_to_parent with test cases from JSON file.
+
+    Args:
+        test_case (dict): Test case from JSON file.
+    """
+    bng_ref_string = test_case["bng_ref_string"]
+    resolution = None if test_case["resolution"] == "NULL" else test_case["resolution"]
+
+    if "expected_exception" in test_case:
+        exception_name = test_case["expected_exception"]["name"]
+        message = (
+            None
+            if test_case["expected_exception"]["name"] == "BNGResolutionError"
+            else test_case["expected_exception"]["message"]
+        )
+
+        # Get exception class from name
+        exception_class = _EXCEPTION_MAP[exception_name]
+        with pytest.raises(exception_class, match=message):
+            bng_to_parent(BNGReference(bng_ref_string), resolution)
+
+    else:
+
+        bng = bng_to_parent(BNGReference(bng_ref_string), resolution)
+        assert bng.bng_ref_formatted == test_case["expected"]["bng_ref_formatted"]
