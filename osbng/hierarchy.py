375bb3d0591923b40e0dac8e281d4f9ebda873f8
kat-1218 <kate.new@os.uk>
2025-01-29 08:52:10 +0000

    feat: add hierarchy module providing functionality for getting hierarchy BNG References.

---
+++ osbng/hierarchy.py
@@ -0,0 +1,10 @@
+"""Provides functionality for getting hierarchy BNG References.
+
+This module supports hierachy functions to traverse the British National Grid.
+
+
+Supported Resolutions:
+    - The module supports the standard BNG resolutions, including 100km, 50km, 10km, 5km, 1km, 500m, 100m, 50m, 10m, 
+    5m and 1m.
+    - These resolutions are validated and normalised using the resolution mapping defined in the 'resolution' module.
+"""
