79d0f8a9db11ef4cfc6c8769fecd1a08fbf3baea
kat-1218 <kate.new@os.uk>
2025-01-29 09:03:05 +0000

    feat: add bng_to_parent function to return a bngreference object that is the parent of the input bngreference object

--- osbng/hierarchy.py
+++ osbng/hierarchy.py
@@ -83,3 +83,68 @@ def bng_to_children(bng_ref: BNGReference, resolution=None) -> list[BNGReference
     )
 
     return bng_refs
+
+
+@_validate_bngreference
+def bng_to_parent(bng_ref: BNGReference, resolution=None) -> BNGReference:
+    """Returns a BNGReference object that is the parent of the input BNGReference object.
+
+    By default, the parent of the BNGReference object is defined as the BNGReference in the next BNG
+    resolution up from the input BNGReference resolution. For example, 50km -> 100km.
+
+    Any valid resolution can be provided as the parent resolution, provided it is greater than the
+    resolution of the input BNGReference.
+
+    Args:
+        bng_ref (BNGReference): The BNGReference object to derive parent from.
+        resolution (int, optional): The resolution of the parent BNGReference. Defaults to None.
+
+    Returns:
+        BNGReference: A BNGReference object that is the parent of the input BNGReference object.
+
+    Raises:
+        BNGHierarchyError: If the resolution of the input BNGReference object is 100km.
+        BNGHierarchyError: If the resolution is less than or equal to the resolution of the input BNGReference object.
+        BNGResolutionError: If an invalid resolution is provided.
+
+    Examples:
+        >>> bng_to_parent(BNGReference("SU 3 6 SW"))
+        BNGReference(bng_ref_formatted=SU 3 6, resolution_label=10km)
+        >>> bng_to_parent(BNGReference("SU 342 567"))
+        BNGReference(bng_ref_formatted=SU 34 56 NW, resolution_label=500m)
+        >>> bng_to_parent(BNGReference("SU 342 567"), resolution=10000)
+        BNGReference(bng_ref_formatted=SU 3 5, resolution_label=10km)
+
+    """
+
+    # Raise error if the resolution is 100km
+    if bng_ref.resolution_metres == 100000:
+        raise BNGHierarchyError(
+            "Cannot derive parent from the coarsest 100km resolution"
+        )
+
+    # Generate a scaled resolution if none is provided
+    if resolution is None:
+        resolution = bng_ref.resolution_metres
+
+        if _RESOLUTION_TO_STRING[resolution]["quadtree"]:
+            resolution = int((bng_ref.resolution_metres * 2))
+        else:
+            resolution = int((bng_ref.resolution_metres * 5))
+
+    # Validate and normalise the resolution to its metre-based integer value
+    validated_resolution = _validate_and_normalise_bng_resolution(resolution)
+
+    # Raise error if the validated resolution is less than the resolution of the input BNGReference object
+    if validated_resolution <= bng_ref.resolution_metres:
+        raise BNGHierarchyError(
+            "Resolution must be greater than the resolution of input BNGReference object"
+        )
+
+    # Dervive coordinates of the grid square bounding box
+    x, y = bng_to_xy(bng_ref, "lower-left")
+
+    # Derive parent BNGReference object from coordinates
+    bng_ref = xy_to_bng(x, y, validated_resolution)
+
+    return bng_ref
