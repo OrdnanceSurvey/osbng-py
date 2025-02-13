db79b613632d8482286536940e8b6a307ed39ea1
Steve Kingston <steve.kingston@os.uk>
2025-02-13 17:55:56 +0000

    docs: add comments to test functions

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -20,20 +20,26 @@ from osbng.utils import _load_test_cases
 # Parameterised test for _validate_and_normalise_bng_resolution function
 @pytest.mark.parametrize(
     "test_case",
+    # Load test cases from JSON file
     _load_test_cases(file_path="./data/indexing_test_cases.json")[
         "_validate_and_normalise_bng_resolution"
     ],
 )
 def test__validate_and_normalise_bng_resolution(test_case):
     """Test _validate_and_normalise_bng_resolution with test cases from JSON file."""
+    # Load test case data
     resolution = test_case["resolution"]
+    # Check if the test case expects an exception
     if "expected_exception" in test_case:
+        # Get exception name from test case
         exception_name = test_case["expected_exception"]["name"]
         # Get exception class from name
         exception_class = _EXCEPTION_MAP[exception_name]
+        # Check that the function raises the expected exception
         with pytest.raises(exception_class):
             _validate_and_normalise_bng_resolution(resolution)
     else:
+        # Check that the function returns the expected result
         assert (
             _validate_and_normalise_bng_resolution(resolution) == test_case["expected"]
         )
@@ -42,18 +48,23 @@ def test__validate_and_normalise_bng_resolution(test_case):
 # Parameterised test for _validate_easting_northing function
 @pytest.mark.parametrize(
     "test_case",
+    # Load test cases from JSON file
     _load_test_cases(file_path="./data/indexing_test_cases.json")[
         "_validate_easting_northing"
     ],
 )
 def test__validate_easting_northing(test_case):
     """Test _validate_and_normalise_bng_resolution with test cases from JSON file."""
+    # Load test case data
     easting = test_case["easting"]
     northing = test_case["northing"]
+    # Check if the test case expects an exception
     if "expected_exception" in test_case:
+        # Get exception name from test case
         exception_name = test_case["expected_exception"]["name"]
         # Get exception class from name
         exception_class = _EXCEPTION_MAP[exception_name]
+        # Check that the function raises the expected exception
         with pytest.raises(exception_class):
             _validate_easting_northing(easting, northing)
     else:
@@ -67,6 +78,7 @@ def test__validate_easting_northing(test_case):
 # Parameterised test for _get_bng_suffix function
 @pytest.mark.parametrize(
     "test_case",
+    # Load test cases from JSON file
     _load_test_cases(file_path="./data/indexing_test_cases.json")["_get_bng_suffix"],
 )
 def test__get_bng_suffix(test_case):
@@ -75,33 +87,43 @@ def test__get_bng_suffix(test_case):
     Args:
         test_case (dict): Test case from JSON file.
     """
+    # Load test case data
     easting = test_case["easting"]
     northing = test_case["northing"]
     resolution = test_case["resolution"]
     expected = test_case["expected"]
+    # Check that the function returns the expected result
     assert _get_bng_suffix(easting, northing, resolution) == expected
 
 
 # Parameterised test for xy_to_bng function
 @pytest.mark.parametrize(
     "test_case",
+    # Load test cases from JSON file
     _load_test_cases(file_path="./data/indexing_test_cases.json")[
         "xy_to_bng"
     ],
 )
 def test_xy_to_bng(test_case):
     """Test xy_to_bng with test cases from JSON file."""
+    # Load test case data
     easting = test_case["easting"]
     northing = test_case["northing"]
     resolution = test_case["resolution"]
+    # Check if the test case expects an exception
     if "expected_exception" in test_case:
+        # Get exception name from test case
         exception_name = test_case["expected_exception"]["name"]
         # Get exception class from name
         exception_class = _EXCEPTION_MAP[exception_name]
+        # Check that the function raises the expected exception
         with pytest.raises(exception_class):
             xy_to_bng(easting, northing, resolution)
     else:
+        # Create BNGReference object
         bng_ref = xy_to_bng(easting, northing, resolution)
+        
+        # Check that the function does not raise any exception
         assert bng_ref.bng_ref_formatted == test_case["expected"]["bng_ref_formatted"]
 
 # Parameterised test for bng_to_xy function
