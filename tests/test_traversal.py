aaf6d8537b11f2a8dba508c75ecdc552511124f6
Steve Kingston <steve.kingston@os.uk>
2025-07-02 17:05:22 +0100

    docs: add test function comments and args section with typeddict class to docstrings in test_traversal.py
    - Added descriptive comments within each test function.
    Updated all test function docstrings to include an Args section specifying the relevant TypedDict test case class.

--- tests/test_traversal.py
+++ tests/test_traversal.py
@@ -34,23 +34,33 @@ class BNGDistanceTestCase(TypedDict):
 # Parameterised test for bng_distance function
 @pytest.mark.parametrize(
     "test_case",
+    # Load test cases from JSON file
     _load_test_cases(file_path="./data/traversal_test_cases.json")["bng_distance"],
 )
 def test_bng_distance(test_case: BNGDistanceTestCase):
-    """Test bng_distance with test cases from JSON file."""
+    """Test bng_distance with test cases from JSON file.
+    
+    Args:
+        test_case (BNGDistanceTestCase): The test case from JSON file.
+    """
     bng_ref1 = test_case["bng_ref_string_1"]
     bng_ref2 = test_case["bng_ref_string_2"]
     if "expected_exception" in test_case:
+        # Get exception name from test case
         exception_name = test_case["expected_exception"]["name"]
         # Get exception class from name
         exception_class = _EXCEPTION_MAP[exception_name]
+        # Assert that the test case raises the expected exception
         with pytest.raises(exception_class):
             bng_distance(BNGReference(bng_ref1), BNGReference(bng_ref2))
     elif "edge_to_edge" in test_case:
+        # If edge_to_edge is specified, use it in the distance calculation
         edge_to_edge = test_case["edge_to_edge"]
+        # Assert that the function returns the expected result
         distance = bng_distance(BNGReference(bng_ref1), BNGReference(bng_ref2), edge_to_edge=edge_to_edge)
         assert distance == test_case["expected"]
     else:
+        # Assert that the function returns the expected result
         distance = bng_distance(BNGReference(bng_ref1), BNGReference(bng_ref2))
         assert distance == test_case["expected"]
 
@@ -74,14 +84,22 @@ class BNGIsNeighbourTestCase(TypedDict):
 # Parameterised test for bng_is_neighbour function
 @pytest.mark.parametrize(
     "test_case",
+    # Load test cases from JSON file
     _load_test_cases(file_path="./data/traversal_test_cases.json")["bng_is_neighbour"],
 )
 def test_bng_is_neighbour(test_case: BNGIsNeighbourTestCase):
-    """Test bng_is_neighbour with test cases from JSON file."""
+    """Test bng_is_neighbour with test cases from JSON file.
+    
+    Args:
+        test_case (BNGIsNeighbourTestCase): The test case from JSON file.
+    """
+    # Load test case data
     bng_ref1 = test_case["bng_ref_string_1"]
     bng_ref2 = test_case["bng_ref_string_2"]
     if "expected_exception" in test_case:
+        # Get exception name from test case
         exception_name = test_case["expected_exception"]["name"]
+        # Get exception message from test case
         message = (
             None
             if test_case["expected_exception"]["name"] == "BNGNeighbourError"
@@ -89,9 +107,11 @@ def test_bng_is_neighbour(test_case: BNGIsNeighbourTestCase):
         )
         # Get exception class from name
         exception_class = _EXCEPTION_MAP[exception_name]
+        # Assert that the test case raises the expected exception
         with pytest.raises(exception_class, match=message):
             bng_is_neighbour(BNGReference(bng_ref1), BNGReference(bng_ref2))
     else:
+        # Assert that the function returns the expected result
         distance = bng_is_neighbour(BNGReference(bng_ref1), BNGReference(bng_ref2))
         assert distance == test_case["expected"]
 
@@ -115,14 +135,20 @@ class BNGKRingTestCase(TypedDict):
 # Parameterised test for bng_kring function
 @pytest.mark.parametrize(
     "test_case",
+    # Load test cases from JSON file
     _load_test_cases(file_path="./data/traversal_test_cases.json")["bng_kring"],
 )
 def test_bng_kring(test_case: BNGKRingTestCase):
-    """Test bng_kring with test cases from JSON file."""
-
+    """Test bng_kring with test cases from JSON file.
+    
+    Args:
+        test_case (BNGKRingTestCase): The test case from JSON file.
+    """
     if "expected_length" in test_case:
+        # Assert that the function returns the expected length
         assert len(bng_kring(BNGReference(test_case["bng_ref_string"]), test_case["k"])) == test_case["expected_length"]
     else:
+        # Assert that the function returns the expected result
         kring = bng_kring(BNGReference(test_case["bng_ref_string"]), test_case["k"])
         assert sorted([r.bng_ref_formatted for r in kring]) == sorted(test_case["expected"]["bng_ref_formatted"])
 
@@ -146,14 +172,20 @@ class BNGKDiscTestCase(TypedDict):
 # Parameterised test for bng_kdisc function
 @pytest.mark.parametrize(
     "test_case",
+    # Load test cases from JSON file
     _load_test_cases(file_path="./data/traversal_test_cases.json")["bng_kdisc"],
 )
 def test_bng_kdisc(test_case: BNGKDiscTestCase):
-    """Test bng_kdisc with test cases from JSON file."""
-
+    """Test bng_kdisc with test cases from JSON file.
+    
+    Args:
+        test_case (BNGKDiscTestCase): The test case from JSON file.
+    """
     if "expected_length" in test_case:
+        # Assert that the function returns the expected length
         assert len(bng_kdisc(BNGReference(test_case["bng_ref_string"]), test_case["k"])) == test_case["expected_length"]
     else:
+        # Assert that the function returns the expected result
         kring = bng_kdisc(BNGReference(test_case["bng_ref_string"]), test_case["k"])
         assert sorted([r.bng_ref_formatted for r in kring]) == sorted(test_case["expected"]["bng_ref_formatted"])
 
@@ -177,14 +209,20 @@ class BNGKDWithinTestCase(TypedDict):
 # Parameterised test for bng_dwithin function
 @pytest.mark.parametrize(
     "test_case",
+    # Load test cases from JSON file
     _load_test_cases(file_path="./data/traversal_test_cases.json")["bng_dwithin"],
 )
 def test_bng_dwithin(test_case: BNGKDWithinTestCase):
-    """Test bng_dwithin with test cases from JSON file."""
-
+    """Test bng_dwithin with test cases from JSON file.
+    
+    Args:
+        test_case (BNGKDWithinTestCase): The test case from JSON file.
+    """
     if "expected_length" in test_case:
+        # Assert that the function returns the expected length
         assert len(bng_dwithin(BNGReference(test_case["bng_ref_string"]), test_case["d"])) == test_case["expected_length"]
     else:
+        # Assert that the function returns the expected result
         kring = bng_dwithin(BNGReference(test_case["bng_ref_string"]), test_case["d"])
         assert sorted([r.bng_ref_formatted for r in kring]) == sorted(test_case["expected"]["bng_ref_formatted"])
 
