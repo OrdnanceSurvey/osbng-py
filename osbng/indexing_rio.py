bc571351de505c76812d37e3bed0f1f5d0ce9172
Lucy Main <lucy.main@os.uk>
2026-01-16 09:03:26 +0000

    feat: add helper to validate raster within extent

--- osbng/indexing_rio.py
+++ osbng/indexing_rio.py
@@ -11,9 +11,31 @@ Note:
 """
 
 try:
-    import rasterio as rio  # noqa: F401, I001
+    import rasterio as rio  # noqa: F401
 except ImportError as e:
     raise ImportError(
         "The 'rasterio' package is required to use the 'osbng.indexing_rio' module. "
         "Install it with: pip install osbng[rasterio]"
     ) from e
+
+from rasterio import DatasetReader
+from shapely.geometry import box
+
+from osbng.errors import BNGExtentError
+
+
+def _validate_within_extent(src: DatasetReader) -> None:
+    """Validates that coordinates are within the bounds of the BNG index system extent.
+
+    Args:
+        src (DatasetReader): A rasterio dataset.
+
+    Raises:
+        BNGExtentError: If the raster bounds are outside the BNG index system extent.
+    """
+    rst_bbox = box(*src.bounds)
+    bng_extent = box(0, 0, 700000, 1300000)
+    if not rst_bbox.within(bng_extent):
+        raise BNGExtentError(
+            "The raster bounds are outside the BNG index system extent."
+        )
